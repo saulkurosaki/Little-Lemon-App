b50237595302c394921b66f9956f5ce63a00f289
Saul Paredes <saulkurosaki13@gmail.com>
2023-05-15 16:52:14 -0600

    Specials Section refactor

---
+++ src/components/SpecialsSection.js
@@ -0,0 +1,26 @@
+import Card from './Card';
+import Button from './Button';
+import './SpecialsSection.css';
+import greekSalad from '../icons_assets/greek salad.jpg';
+import bruchetta from '../icons_assets/bruchetta.svg';
+import lemonDessert from '../icons_assets/lemon dessert.jpg';
+
+const SpecialsSection = () => {
+    return(
+        <section className='specials'>
+            <article>
+                <div className="banner">
+                    <p>This Week Specials!</p>
+                    <Button>Online Menu</Button>
+                </div>
+                <div className='cards'>
+                    <Card name='Greek Salad' price='$12.99' img={greekSalad}>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic an rosemary croutons.</Card>
+                    <Card name='Bruchetta' price='$5.99' img={bruchetta}>Our Bruchetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</Card>
+                    <Card name='Lemon Dessert' price='$5.00' img={lemonDessert}>This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</Card>
+                </div>
+            </article>
+        </section>
+    );
+};
+
+export default SpecialsSection;
