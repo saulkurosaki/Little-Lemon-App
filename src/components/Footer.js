157b1c361846a713ead73250026fd0f9dcdfdab5
Saul Paredes <saulkurosaki13@gmail.com>
2023-05-17 16:09:28 -0600

    Footer section completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,42 @@
+import './Footer.css';
+import footerLogo from '../icons_assets/Little Lemon Footer Green.jpg';
+
 const Footer = () => {
     return(
-        <>
-            <footer>
-                <h3>Soy el Pie de pagina</h3>
-            </footer>
-        </>
+        <footer className='footer'>
+            <div className='container'>
+                <div id='footer-logo'>
+                    <img src={footerLogo} alt='Logo'/>
+                </div>
+                <div className='columns'>
+                    <ul>
+                        <li><h3>Doormat Navigation</h3></li>
+                        <li><a>Home</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Menu</a></li>
+                        <li><a>Reservations</a></li>
+                        <li><a>Order Online</a></li>
+                        <li><a>Login</a></li>
+                    </ul>
+                </div>
+                <div className='columns'>
+                    <ul>
+                        <li><h3>Contact</h3></li>
+                        <li><a>Address</a></li>
+                        <li><a>Phone Number</a></li>
+                        <li><a>Email</a></li>
+                    </ul>
+                </div>
+                <div className='columns'>
+                    <ul>
+                        <li><h3>Social Media Links</h3></li>
+                        <li><a>Address</a></li>
+                        <li><a>Phone Number</a></li>
+                        <li><a>Email</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
     );
 };
 
